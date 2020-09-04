1c9bb6539fc2b4eb8cd8b10a6fe6a7f4c5da416c
sofia toivola <sofia.toivola@kuuasema.com>
2020-09-04 16:35:25 +0300

    add multiplayer

---
+++ Assets/Multiplayer/MultiplayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Multiplayer
+{
+    public class MultiplayerController : MonoBehaviour
+    {
+        [SerializeField] private GameObject bike;
+
+        private void OnJoinedRoom()
+        {
+            if (bike.activeSelf) bike.SetActive(false);
+            CreatePlayerObject();
+        }
+
+        private void CreatePlayerObject()
+        {
+            var position = bike.transform.position + Vector3.up;
+
+            PhotonNetwork.Instantiate(bike.gameObject.name, position, Quaternion.identity, 0);
+        }
+    }
+}
