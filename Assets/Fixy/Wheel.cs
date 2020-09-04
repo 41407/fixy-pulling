e75f32db905795534ccc716e44a6e34f83190bdc
Jimi W <jimi.welling@kuuasema.com>
2020-09-04 16:26:54 +0300

    Demagicify wheel rotation speed calculation

--- Assets/Fixy/Wheel.cs
+++ Assets/Fixy/Wheel.cs
@@ -5,21 +5,22 @@ namespace Fixy
 {
     public interface IWheel
     {
-        void SetSpeed(float kmh);
+        void SetSpeed(float unitsPerSecond);
     }
 
     public class Wheel : MonoBehaviour, IWheel
     {
         private float CurrentAngularSpeed { get; set; }
+        private float WheelDiameter => transform.localScale.y;
 
         void FixedUpdate()
         {
             transform.Rotate(Vector3.right, CurrentAngularSpeed * Time.fixedDeltaTime);
         }
 
-        public void SetSpeed(float kmh)
+        public void SetSpeed(float unitsPerSecond)
         {
-            CurrentAngularSpeed = kmh * 200f;
+            CurrentAngularSpeed = unitsPerSecond * WheelDiameter * 100f * Mathf.PI;
         }
     }
 }
